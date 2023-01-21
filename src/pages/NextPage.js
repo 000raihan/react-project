c077f8a7437766b19195f8c48229538f7cfd112d
Tareq <tareq236@gmail.com>
2023-01-21 20:02:23 +0600

    tareq

--- src/pages/NextPage.js
+++ src/pages/NextPage.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-
+import VideoRecorder from 'react-video-recorder'
 const NextPage = () => {
   return (
     <>
         <div className='container-fluid'>
             <div className='row'>
-                test
+                <VideoRecorder
+                    chunkSize={250}
+                    constraints={{
+                        audio: true,
+                        video: true
+                    }}
+                    countdownTime={3000}
+                    dataAvailableTimeout={500}
+                    isFlipped
+                    mimeType={undefined}
+                    onError={function noRefCheck(){}}
+                    onOpenVideoInput={function noRefCheck(){}}
+                    onRecordingComplete={function noRefCheck(){}}
+                    onStartRecording={function noRefCheck(){}}
+                    onStopRecording={function noRefCheck(){}}
+                    onStopReplaying={function noRefCheck(){}}
+                    onTurnOffCamera={function noRefCheck(){}}
+                    onTurnOnCamera={function noRefCheck(){}}
+                    renderActions={function noRefCheck(){}}
+                    renderDisconnectedView={function noRefCheck(){}}
+                    renderErrorView={function noRefCheck(){}}
+                    renderLoadingView={function noRefCheck(){}}
+                    renderUnsupportedView={function noRefCheck(){}}
+                    renderVideoInputView={function noRefCheck(){}}
+                    t={function noRefCheck(){}}
+                    timeLimit={undefined}
+                />
             </div>
         </div>
     </>
