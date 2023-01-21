d8c0b7595baceac146ed345817b99b255697e053
Tareq <tareq236@gmail.com>
2023-01-21 19:01:35 +0600

    tareq

--- src/pages/FirstPage.js
+++ src/pages/FirstPage.js
@@ -1,15 +1,49 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import { NavLink } from 'react-router-dom'
 
-const FirstPage = () => {
-  return (
-    <div>
-        <h1>This is FirstPage</h1>
-        <NavLink to='/next-page'>
-            Go to NextPage
-        </NavLink>
-        </div>
-  )
+import video_1 from '../assets/video_1.mp4';
+import video_2 from '../assets/video_2.mp4';
+
+class FirstPage extends React.Component {
+    state = {v_file: video_1};
+
+    componentDidMount = () => {
+        // this.playVideo();
+    };
+    componentWillUnmount = () => {
+        // this.pauseVideo();
+    };
+    playVideo1 = () => {
+        this.setState({ v_file: video_1 });
+        this.refs.vidRef.play();
+    };
+    playVideo2 = () => {
+        this.setState({ v_file: video_2 });
+        this.refs.vidRef.play();
+    };
+
+    render = () => {
+        return (
+            <div className='container-fluid'>
+                <div className='row'>
+                    <video
+                        ref="vidRef"
+                        src={this.state.v_file}
+                        type="video/mp4"
+                    />
+
+                    <div>
+                        <button onClick={this.playVideo1}>
+                            Play 1
+                        </button>
+                        <button onClick={this.playVideo2}>
+                            Play 2
+                        </button>
+                    </div>
+                </div>
+            </div>
+        );
+    };
 }
 
 export default FirstPage
--- src/pages/NextPage.js
+++ src/pages/NextPage.js
@@ -3,12 +3,13 @@ import { NavLink } from 'react-router-dom';
 
 const NextPage = () => {
   return (
-    <div> 
-        <h1>This is  NextPage</h1>
-        <NavLink to='/'>
-            Go to First Page
-        </NavLink>
+    <>
+        <div className='container-fluid'>
+            <div className='row'>
+                test
+            </div>
         </div>
+    </>
   )
 }
 
